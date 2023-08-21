067e327df882c05a5ad028a1ccea4ba87daab77a
jasim khan <jasim.khan@streamstech.com>
2023-08-21 11:08:15 +0600

    update counter service

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core'
 import { StateController, Action } from './store'
 import { IAppService, Todo } from './app.service.types'
 import { AppService } from './app.service'
-import { map, mergeMap, withLatestFrom } from 'rxjs'
+import { delay, map, mergeMap, withLatestFrom } from 'rxjs'
 
 @Injectable({ providedIn: 'root' })
 export class CounterService extends StateController<number>{
@@ -10,12 +10,30 @@ export class CounterService extends StateController<number>{
         super(0)
     }
 
+    override onInit(): void {
+        this.tearDown(
+            this.remoteController(AppService).pipe(
+                mergeMap(service => service.select(state => state.todos.length))
+            ).subscribe(len => this.emit(len))
+        )
+        /*this.effectOnAction(
+            this.action$.whereType('inc').pipe(
+                delay(1300),
+                withLatestFrom(
+                    this.remoteStream<IAppService>(AppService).pipe(map(state=>state.todos.length))
+                    ),
+                    map(([_,len])=>len)
+            )
+        )*/
+
+    }
+
     override onAction(action: Action): void {
-        if(action.type==='inc'){
-            this.emit(this.state+1)
+        if (action.type === 'inc') {
+            this.emit(this.state + 1)
         }
-        else if(action.type==='dec'){
-            this.emit(this.state-1)
+        else if (action.type === 'dec') {
+            this.emit(this.state - 1)
         }
     }
 }
